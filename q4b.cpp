a820a9aa5e1d5aa86f1ce100dd2265d4e16f10f9
himasri11 <hiskiya.chadalavada@gmail.com>
2025-08-20 15:05:22 +0530

    Create q4b.cpp

---
+++ q4b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A[2][3] = {{1, 2, 3},
+                   {4, 5, 6}};
+    int B[3][2] = {{7, 8},
+                   {9, 10},
+                   {11, 12}};
+
+    int result[2][2] = {0};
+
+    // Multiply A and B
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            for(int k = 0; k < 3; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    cout << "Result of matrix multiplication:\n";
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
